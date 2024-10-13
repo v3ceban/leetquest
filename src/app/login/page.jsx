5651917ad1be0ea5024b98af6d99ca496e24deab
Matty Herzig <mattyherzig@yahoo.com>
2024-10-12 20:07:45 -0700

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,15 @@
 export default function Login() {
   return (
-    <div className="flex justify-center">
-      <div>
-        {/* TODO: Login Div */}
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col rounded w-[30ch] h-[10em] overflow-hidden">
+        <div className="flex justify-between text-white bg-gray-800 [&>*]:px-4">
+          <div>Login</div>
+          <div>?</div>
+        </div>
+        <div className="bg-gray-500 h-full flex justify-center items-center gap-4">
+          <div>Google</div>
+          <div>GitHub</div>
+        </div>
       </div>
     </div>
   );

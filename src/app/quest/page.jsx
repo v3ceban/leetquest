db50c2f08b1bc78795cc826c818a09f8868309f1
Matty Herzig <mattyherzig@yahoo.com>
2024-10-13 23:14:13 -0700

    world data

--- src/app/quest/page.jsx
+++ src/app/quest/page.jsx
@@ -3,7 +3,15 @@
 import { useState, useEffect } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { getBoxToBoxArrow } from "perfect-arrows";
-import worlds from '@/data/worlds/worlds.json';
+
+import worldsData from '@/data/worlds.json';
+import arrayWorldData from '@/data/worlds/array.json';
+// import hashingWorldData from '@/data/worlds/hashing.json';
+
+const worldData = {
+  "Array": arrayWorldData,
+  // "Hashing": hashingWorldData
+};
 
 const WORLD_WIDTH = 70;
 const WORLD_HEIGHT = 25;
@@ -73,9 +81,49 @@ export default function Quest() {
         <div
           className={`absolute top-0 right-0 w-2/3 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 ${isVisible ? 'translate-x-0' : 'translate-x-[110%]'}`}
         >
-          <div className="p-4">
-            <h2 className="text-2xl font-bold">{worlds[selectedWorld].name}</h2>
-            {/* TODO: World map */}
+          <div>
+            <h2 className="text-2xl font-bold p-4">{selectedWorld}</h2>
+            <TransformWrapper>
+              <TransformComponent>
+                <div className="relative w-screen h-screen">
+                  {worldData[selectedWorld] && Object.entries(worldData[selectedWorld]).map(([name, { x, y }]) => (
+                    <div
+                      key={name}
+                      className="absolute text-white bg-gray-800 rounded flex justify-center cursor-pointer"
+                      style={{ left: x, top: y, width: WORLD_WIDTH, height: WORLD_HEIGHT }}
+                    >
+                      {name}
+                    </div>
+                  ))}
+                  <svg
+                    className="w-full h-full"
+                    stroke="#1F2937"
+                    fill="#1F2937"
+                    strokeWidth={3}
+                  >
+                    {worldData[selectedWorld] && Object.entries(worldData[selectedWorld]).flatMap(([name, { x, y, prereqs }]) =>
+                      prereqs.map(({ name: prereq, flip_arrow: flipArrow }) => {
+                        const prereqWorld = worldData[selectedWorld][prereq];
+                        if (!prereqWorld) {
+                          return null;
+                        }
+                        const [sx, sy, cx, cy, ex, ey, ae] = getArrow(x, y, prereqWorld.x, prereqWorld.y, flipArrow);
+                        const endAngleAsDegrees = ae * (180 / Math.PI);
+                        return (
+                          <g key={`${name}-${prereq}`}>
+                            <path d={`M${sx},${sy} Q${cx},${cy} ${ex},${ey}`} fill="none" />
+                            <polygon
+                              points="0,-6 12,0, 0,6"
+                              transform={`translate(${ex},${ey}) rotate(${endAngleAsDegrees})`}
+                            />
+                          </g>
+                        );
+                      })
+                    )}
+                  </svg>
+                </div>
+              </TransformComponent>
+            </TransformWrapper>
           </div>
           <div
             className="absolute top-1/2 left-0 -translate-x-full -translate-y-1/2 cursor-pointer"
@@ -93,12 +141,12 @@ export default function Quest() {
       <TransformWrapper>
         <TransformComponent>
           <div className="w-screen h-screen">
-            {Object.entries(worlds).map(([id, { name, x, y }]) => (
+            {Object.entries(worldsData).map(([name, { x, y }]) => (
               <div
-                key={id}
+                key={name}
                 className="absolute text-white bg-gray-800 rounded flex justify-center cursor-pointer"
                 style={{ left: x, top: y, width: WORLD_WIDTH, height: WORLD_HEIGHT }}
-                onClick={() => handleWorldClick(id)}
+                onClick={() => handleWorldClick(name)}
               >
                 {name}
               </div>
@@ -109,16 +157,16 @@ export default function Quest() {
               fill="#1F2937"
               strokeWidth={3}
             >
-              {Object.entries(worlds).flatMap(([id, { x, y, prereqs }]) =>
-                prereqs.map(({ id: prereqId, flip_arrow: flipArrow }) => {
-                  const prereqWorld = worlds[prereqId];
+              {Object.entries(worldsData).flatMap(([name, { x, y, prereqs }]) =>
+                prereqs.map(({ name: prereq, flip_arrow: flipArrow }) => {
+                  const prereqWorld = worldsData[prereq];
                   if (!prereqWorld) {
                     return null;
                   }
                   const [sx, sy, cx, cy, ex, ey, ae] = getArrow(x, y, prereqWorld.x, prereqWorld.y, flipArrow);
                   const endAngleAsDegrees = ae * (180 / Math.PI);
                   return (
-                    <g key={`${id}-${prereqId}`}>
+                    <g key={`${name}-${prereq}`}>
                       <path d={`M${sx},${sy} Q${cx},${cy} ${ex},${ey}`} fill="none" />
                       <polygon
                         points="0,-6 12,0, 0,6"

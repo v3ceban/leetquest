cc969b60cd591718c9048cd010c6331237e4bd7b
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-10-14 17:11:00 -0700

    fix main max-h

--- src/app/quest/page.jsx
+++ src/app/quest/page.jsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { ArrowsWrapper, Arrow } from '@/components/Arrow';
-import { World, WorldNode } from '@/components/World';
+import React, { useState, useEffect } from "react";
+import { ArrowsWrapper, Arrow } from "@/components/Arrow";
+import { World, WorldNode } from "@/components/World";
 
-import worldsData from '@/data/worlds.json';
-import arrayWorldData from '@/data/worlds/array.json';
+import worldsData from "@/data/worlds.json";
+import arrayWorldData from "@/data/worlds/array.json";
 // import hashingWorldData from '@/data/worlds/hashing.json';
 
 const worldData = {
-  "Worlds": worldsData,
-  "Array": arrayWorldData,
+  Worlds: worldsData,
+  Array: arrayWorldData,
   // "Hashing": hashingWorldData
 };
 
@@ -45,21 +45,84 @@ export default function Quest() {
   }, [selectedWorld]);
 
   return (
-    <div className="relative">
+    <main className="relative h-[calc(100dvh-72px)]">
       {/* TODO: Breadcrumb */}
 
       {selectedWorld && (
         <div
-          className={`absolute top-0 right-0 w-2/3 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 ${isVisible ? 'translate-x-0' : 'translate-x-[110%]'}`}
+          className={`absolute top-0 right-0 w-2/3 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 ${isVisible ? "translate-x-0" : "translate-x-[110%]"}`}
         >
           <World title={selectedWorld}>
-            {worldData[selectedWorld] && Object.entries(worldData[selectedWorld]).map(([name, { x, y, color }]) => (
-              <WorldNode key={name} isAWorld={false} name={name} levelColor={color} x={x} y={y} onClick={() => {}} />
-            ))}
+            {worldData[selectedWorld] &&
+              Object.entries(worldData[selectedWorld]).map(
+                ([name, { x, y, color }]) => (
+                  <WorldNode
+                    key={name}
+                    isAWorld={false}
+                    name={name}
+                    levelColor={color}
+                    x={x}
+                    y={y}
+                    onClick={() => { }}
+                  />
+                ),
+              )}
             <ArrowsWrapper>
-              {worldData[selectedWorld] && Object.entries(worldData[selectedWorld]).flatMap(([name, { x, y, prereqs = {} }]) =>
-                Object.entries(prereqs).map(([prereq, { flip_arrow: flipArrow }]) => {
-                  const prereqWorld = worldData[selectedWorld][prereq];
+              {worldData[selectedWorld] &&
+                Object.entries(worldData[selectedWorld]).flatMap(
+                  ([name, { x, y, prereqs = {} }]) =>
+                    Object.entries(prereqs).map(
+                      ([prereq, { flip_arrow: flipArrow }]) => {
+                        const prereqWorld = worldData[selectedWorld][prereq];
+                        if (!prereqWorld) {
+                          return null;
+                        }
+                        return (
+                          <Arrow
+                            key={`${name}-${prereq}`}
+                            x1={prereqWorld.x}
+                            y1={prereqWorld.y}
+                            x2={x}
+                            y2={y}
+                            isAWorld={false}
+                            flipArrow={flipArrow || false}
+                          />
+                        );
+                      },
+                    ),
+                )}
+            </ArrowsWrapper>
+          </World>
+          <div
+            className="absolute left-0 top-1/2 -translate-x-full -translate-y-1/2 cursor-pointer"
+            style={{
+              borderBottom: "30px solid transparent",
+              borderTop: "30px solid transparent",
+              borderRight: "30px solid #1F2937",
+              height: "80px",
+            }}
+            onClick={closeWindow}
+          />
+        </div>
+      )}
+
+      <World title={"Worlds"}>
+        {Object.entries(worldData["Worlds"]).map(([name, { x, y }]) => (
+          <WorldNode
+            key={name}
+            isAWorld={true}
+            name={name}
+            x={x}
+            y={y}
+            onClick={() => handleWorldClick(name)}
+          />
+        ))}
+        <ArrowsWrapper>
+          {Object.entries(worldData["Worlds"]).flatMap(
+            ([name, { x, y, prereqs = {} }]) =>
+              Object.entries(prereqs).map(
+                ([prereq, { flip_arrow: flipArrow }]) => {
+                  const prereqWorld = worldData["Worlds"][prereq];
                   if (!prereqWorld) {
                     return null;
                   }
@@ -70,53 +133,15 @@ export default function Quest() {
                       y1={prereqWorld.y}
                       x2={x}
                       y2={y}
-                      isAWorld={false}
+                      isAWorld={true}
                       flipArrow={flipArrow || false}
                     />
                   );
-                })
-              )}
-            </ArrowsWrapper>
-          </World>
-          <div
-            className="absolute top-1/2 left-0 -translate-x-full -translate-y-1/2 cursor-pointer"
-            style={{
-              borderBottom: '30px solid transparent',
-              borderTop: '30px solid transparent',
-              borderRight: '30px solid #1F2937',
-              height: '80px',
-            }}
-            onClick={closeWindow}
-          />
-        </div>
-      )}
-
-      <World title={"Worlds"}>
-        {Object.entries(worldData["Worlds"]).map(([name, { x, y }]) => (
-          <WorldNode key={name} isAWorld={true} name={name} x={x} y={y} onClick={() => handleWorldClick(name)} />
-        ))}
-        <ArrowsWrapper>
-          {Object.entries(worldData["Worlds"]).flatMap(([name, { x, y, prereqs = {} }]) =>
-            Object.entries(prereqs).map(([prereq, { flip_arrow: flipArrow }]) => {
-              const prereqWorld = worldData["Worlds"][prereq];
-              if (!prereqWorld) {
-                return null;
-              }
-              return (
-                <Arrow
-                  key={`${name}-${prereq}`}
-                  x1={prereqWorld.x}
-                  y1={prereqWorld.y}
-                  x2={x}
-                  y2={y}
-                  isAWorld={true}
-                  flipArrow={flipArrow || false}
-                />
-              );
-            })
+                },
+              ),
           )}
         </ArrowsWrapper>
       </World>
-    </div>
+    </main>
   );
 }
--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const WORLD_WIDTH = 70;
@@ -5,21 +6,19 @@ const WORLD_HEIGHT = 25;
 const LEVEL_RADIUS = 20;
 const LEVEL_DIAMETER = LEVEL_RADIUS * 2;
 const LEVEL_COLOR_TO_BACKGROUND_COLOR = {
-  'blue': 'bg-blue-700',
-  'green': 'bg-green-600',
-  'red': 'bg-red-600',
-  'default': 'bg-gray-800',
-}
+  blue: "bg-blue-700",
+  green: "bg-green-600",
+  red: "bg-red-600",
+  default: "bg-gray-800",
+};
 
 function World({ children, title }) {
   return (
     <div>
-      <h2 className="text-2xl font-bold p-4">{title}</h2>
+      <h2 className="p-4 text-2xl font-bold">{title}</h2>
       <TransformWrapper>
         <TransformComponent>
-          <div className="relative w-screen h-screen">
-            {children}
-          </div>
+          <div className="relative w-screen h-full">{children}</div>
         </TransformComponent>
       </TransformWrapper>
     </div>
@@ -30,7 +29,7 @@ function WorldNode({ isAWorld, name, levelColor, x, y, onClick }) {
   return isAWorld ? (
     <div
       key={name}
-      className="absolute text-white bg-gray-800 rounded flex justify-center items-center cursor-pointer"
+      className="flex absolute justify-center items-center text-white bg-gray-800 rounded cursor-pointer"
       style={{ left: x, top: y, width: WORLD_WIDTH, height: WORLD_HEIGHT }}
       onClick={onClick}
     >
@@ -39,7 +38,7 @@ function WorldNode({ isAWorld, name, levelColor, x, y, onClick }) {
   ) : (
     <div
       key={name}
-        className={`absolute text-white rounded flex justify-center items-center cursor-pointer rounded-full ${LEVEL_COLOR_TO_BACKGROUND_COLOR[levelColor] || LEVEL_COLOR_TO_BACKGROUND_COLOR["default"]}`}
+      className={`absolute text-white flex justify-center items-center cursor-pointer rounded-full ${LEVEL_COLOR_TO_BACKGROUND_COLOR[levelColor] || LEVEL_COLOR_TO_BACKGROUND_COLOR["default"]}`}
       style={{ left: x, top: y, width: LEVEL_DIAMETER, height: LEVEL_DIAMETER }}
     >
       {name}

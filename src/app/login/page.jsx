807892999c78342e4f00dea0698f572b57d36491
Matty Herzig <mattyherzig@yahoo.com>
2024-10-14 17:26:26 -0700

    login page height

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,6 @@
 export default function Login() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[calc(100dvh-72px)]">
       <div className="flex flex-col rounded w-[30ch] h-[10em] overflow-hidden">
         <div className="flex justify-between text-white bg-gray-800 [&>*]:px-4">
           <div>Login</div>

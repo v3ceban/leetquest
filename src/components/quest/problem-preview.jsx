db296971892686ededb05dadeb202c8e0ed0a47c
Matty Herzig <mattyherzig@yahoo.com>
2025-04-18 16:47:37 -0700

    hover, code border, and content scroll

--- src/components/quest/problem-preview.jsx
+++ src/components/quest/problem-preview.jsx
@@ -78,13 +78,10 @@ const ProblemPreview = () => {
         </div>
         <h2 className="w-2/3 text-2xl">{selectedLevelData.title}</h2>
       </div>
-      <div>
-        
       <div
-        className="prose prose-invert overflow-scroll"
+        className="overflow-scroll prose prose-invert"
         dangerouslySetInnerHTML={{ __html: selectedLevelData.description }}
       />
-      </div>
       <div
         className={cn(
           "grid gap-4 mx-auto mt-auto w-fit",

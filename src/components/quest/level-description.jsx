19a9475462d7545865bbee2d53d22ccd869683af
Matty Herzig <mattyherzig@yahoo.com>
2025-04-24 04:07:25 -0700

    nvm dont have to replace colors

--- src/components/quest/level-description.jsx
+++ src/components/quest/level-description.jsx
@@ -1,44 +1,44 @@
 import parse from "html-react-parser";
 import CodeTrier from "./code-trier";
 
-const Green = ({ children }) => (
-  <span style={{ color: "lightgreen" }}>{children}</span>
-);
+// const Green = ({ children }) => (
+//   <span style={{ color: "lightgreen" }}>{children}</span>
+// );
 
-const Red = ({ children }) => (
-  <span style={{ color: "lightcoral" }}>{children}</span>
-);
+// const Red = ({ children }) => (
+//   <span style={{ color: "lightcoral" }}>{children}</span>
+// );
 
-const Orange = ({ children }) => (
-  <span style={{ color: "rgb(255, 178, 102)" }}>{children}</span>
-);
+// const Orange = ({ children }) => (
+//   <span style={{ color: "rgb(255, 178, 102)" }}>{children}</span>
+// );
 
-const Purple = ({ children }) => (
-  <span style={{ color: "rgb(200, 160, 238)" }}>{children}</span>
-);
+// const Purple = ({ children }) => (
+//   <span style={{ color: "rgb(200, 160, 238)" }}>{children}</span>
+// );
 
-const Yellow = ({ children }) => (
-  <span style={{ color: "rgb(240, 240, 163)" }}>{children}</span>
-);
+// const Yellow = ({ children }) => (
+//   <span style={{ color: "rgb(240, 240, 163)" }}>{children}</span>
+// );
 
-const Blue = ({ children }) => (
-  <span style={{ color: "rgb(151, 217, 238)" }}>{children}</span>
-);
+// const Blue = ({ children }) => (
+//   <span style={{ color: "rgb(151, 217, 238)" }}>{children}</span>
+// );
 
-const colorComponents = {
-  green: Green,
-  red: Red,
-  orange: Orange,
-  purple: Purple,
-  yellow: Yellow,
-  blue: Blue,
-};
+// const colorComponents = {
+//   green: Green,
+//   red: Red,
+//   orange: Orange,
+//   purple: Purple,
+//   yellow: Yellow,
+//   blue: Blue,
+// };
 
 const replacePlaceholders = (node) => {
-  if (node.type === "tag" && colorComponents[node.name]) {
-    const Component = colorComponents[node.name.toLowerCase()];
-    return <Component>{node.children.map((child) => replacePlaceholders(child) || child.data)}</Component>;
-  }
+  // if (node.type === "tag" && colorComponents[node.name]) {
+  //   const Component = colorComponents[node.name.toLowerCase()];
+  //   return <Component>{node.children.map((child) => replacePlaceholders(child) || child.data)}</Component>;
+  // }
   if (node.type === "tag" && node.name === "codetrier") {
     const defaultEditorValue = node.attribs.defaulteditorvalue;
     console.log("defaultEditorValue", defaultEditorValue);
